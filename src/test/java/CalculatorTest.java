f11007b3b4fae391cd818e6f4adb89db578f1f2a
KIMYIJI <yiji_kim@yonsei.ac.kr>
2026-03-31 17:59:35 +0900

    update: testcode implementation

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,48 @@
-import org.assertj.core.api.Assertions.*; //assertj
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
+    Adder adder = mock(Adder.class);
+    Calculator calculator = new Calculator(adder);
 
+    @BeforeEach
+    void setUp() {
+        doAnswer(invocation -> {
+            int value = invocation.getArgument(0);
+            return value + 1;
+        }).when(adder).add(anyInt(), eq(1));
+
+        doAnswer(invocation -> {
+            int result = invocation.getArgument(0);
+            int value = invocation.getArgument(1);
+            return result + value;
+        }).when(adder).add(anyInt(), anyInt());
+
+//        System.out.println(adder.add(1, 3));
+    }
+
+    @Test
+    void addTest() {
+        int result = calculator.add(1, 2);
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void subtractTest() {
+        int result = calculator.subtract(1, 2);
+        assertThat(result).isEqualTo(-1);
+    }
+
+    @Test
+    void multiplyTest() {
+        int result = calculator.multiply(1, 2);
+        assertThat(result).isEqualTo(2);
+    }
 }
